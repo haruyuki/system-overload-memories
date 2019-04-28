688713645b73284cd8281c9dc7e909cfd08e648f
haruyuki <jumpy12359@gmail.com>
2019-04-28 11:47:34 +1000

    Update

--- assets/js/server-status.js
+++ assets/js/server-status.js
@@ -1,5 +1,6 @@
-$.getJSON("https://api.mcsrvstat.us/2/sopixelmon.com", function(server) {
-	// To test other functionalities of this function replace link above with
+$.getJSON("https://api.mcsrvstat.us/2/play.rexkraft.com", function(server) {
+	// To test other functionalities of this function replace link above with either
+    // either one of the links below.
 	// https://api.mcsrvstat.us/2/arcadiaeco.us.to
     // https://api.mcsrvstat.us/2/play.rexkraft.com
 	var online = document.getElementById("online");
@@ -29,33 +30,39 @@ $.getJSON("https://api.mcsrvstat.us/2/sopixelmon.com", function(server) {
     	online_players.textContent = "N/A";
     } else {
     	online_players.innerHTML = "<br>";
-    	var documentFragment = document.createDocumentFragment(); // Create a virtual node/tag
+    	var documentFragment = document.createDocumentFragment(); // Create a virtual node
     	var total_players = server.players.list.length;
     	var player_list = server.players.list;
 
     	for (var i = 0; i < total_players; i += 1) {
-    		var img_tag = document.createElement("img");
-    		var l = document.createAttribute("title");
-            l.value = player_list[i];
-            img_tag.className = "player-head";
-            img_tag.src = "http://cravatar.eu/helmavatar/" + player_list[i] + "/30.png";
-            img_tag.setAttributeNode(l);
-            documentFragment.appendChild(img_tag);
+    		var img_tag = document.createElement("img"); // Create an img tag
+    		var title_attribute = document.createAttribute("title");  // Create a title attribute
+            title_attribute.value = player_list[i]; // Set the title attribute to the i'th player name
+            img_tag.className = "player-head"; // Apply 'player-head' class to img tag
+            img_tag.src = "http://cravatar.eu/helmavatar/" + player_list[i] + "/30.png";  // Set img src to a 30px x 30px image of the player's head
+            img_tag.setAttributeNode(title_attribute);  // Apply the title attribute to img tag
+            documentFragment.appendChild(img_tag); // Add img tag to virtual node
         }
-        document.getElementById("online-players").appendChild(documentFragment);
+        document.getElementById("online-players").appendChild(documentFragment); // Append virtual node to online-players div
     }
 });
 
+
+// Taken and modified from https://jqueryui.com/tooltip/#custom-style
 $(function() {
 	$("#online-players").tooltip({
-		show:false,
-		hide:false,
+		show:false, // Disable fade-in transition
+		hide:false, // Disable fade-out transition
 		position: {
-			my:"center bottom-5",
-			at:"center top",
+			my:"center bottom-5", // Align icon to bottom of tooltip
+			at:"center top", // Place tooltip on top of icon
 			using: function(position, feedback) {
 				$(this).css(position);
-				$("<div>").addClass("arrow").addClass(feedback.vertical).addClass(feedback.horizontal).appendTo(this);
+				$("<div>")
+                .addClass("arrow")
+                .addClass(feedback.vertical)
+                .addClass(feedback.horizontal)
+                .appendTo(this);
 			}
 		}
 	});
